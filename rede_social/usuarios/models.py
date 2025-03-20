4070e95b05c629742c20fcd083c2b08718507d49
vanderbraga <vanderbraga34@gmail.com>
2025-03-20 15:53:24 -0300

    Primeiro commit

---
+++ rede_social/usuarios/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    senha = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
